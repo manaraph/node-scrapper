e5769d4102b4f05c95be1fbe73a58d4223ad45b7
manaraph <manaraph225@gmail.com>
2021-01-07 14:41:03 +0100

    Fix findby date error

--- controllers/index.js
+++ controllers/index.js
@@ -2,54 +2,51 @@ const { salesRepo } = require('../services');
 const { getSales, saveSales } = require('../services/util');
 
 const getDailySales = async (req, res) => {
-	const data = await getSales();
-	res.status(200).json({
-		status: 'success',
-		data,
-	});
+  const data = await getSales();
+  res.status(200).json({
+    status: 'success',
+    data,
+  });
 };
 
 const createDailySale = async (req, res) => {
-	const { ps5Sales, xboxSales, date, responseTime } = req.body
-	const { data } = await saveSales(ps5Sales, xboxSales, date, responseTime);
-	return res.status(200).json({
-		status: 'success',
-		data,
-	});
+  const { ps5Sales, xboxSales, date, responseTime } = req.body;
+  const { data } = await saveSales(ps5Sales, xboxSales, date, responseTime);
+  return res.status(200).json({
+    status: 'success',
+    data,
+  });
 };
 
 const getAllSales = async (req, res) => {
-	const data = await salesRepo.getAll();
-	res.status(200).json({
-		status: 'success',
-		data,
-	});
-}
+  const data = await salesRepo.getAll();
+  res.status(200).json({
+    status: 'success',
+    data,
+  });
+};
 
-const getSalesByDate= async (req, res) => {
-	const {
-		date
-	} = req.params;
-
-	const data = await salesRepo.getSalesByDate(date);
-	res.status(200).json({
-		status: 'success',
-		data,
-	});
-}
+const getSalesByDate = async (req, res) => {
+  const { date } = req.query;
+  const data = await salesRepo.getSalesByDate(date);
+  res.status(200).json({
+    status: 'success',
+    data,
+  });
+};
 
 const eraseSales = async (req, res) => {
-	const data = await salesRepo.erase();
-	res.status(200).json({
-		status: 'success',
-		data,
-	});
-}
+  const data = await salesRepo.erase();
+  res.status(200).json({
+    status: 'success',
+    data,
+  });
+};
 
 module.exports = {
-	getDailySales,
-	createDailySale,
-	getAllSales,
-	getSalesByDate,
-	eraseSales,
-};;
+  getDailySales,
+  createDailySale,
+  getAllSales,
+  getSalesByDate,
+  eraseSales,
+};
